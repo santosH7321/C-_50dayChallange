ff28831f74f860a7b41d03725883139434dd3de9
SANTOSH KUMAR <116185780+santosH7321@users.noreply.github.com>
2023-07-21 22:20:16 +0530

    Day_4

---
+++ areaOfCircleusingFunction.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+float circle_area (float radius){
+    float area = 3.14 * radius * radius;
+    return area ;
+}
+int main(){
+    float radius;
+    cout<<"Enter radius: ";
+    cin>>radius;
+    float area = circle_area(radius);
+    cout<<"Area of circle is: "<<area<<endl;
+}
