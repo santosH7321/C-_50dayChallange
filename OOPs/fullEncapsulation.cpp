2da40299a1923ab1772346ec8971caa26eb50033
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-01-07 00:40:47 +0530

    inheritance

---
+++ OOPs/fullEncapsulation.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class Animal {
+    private:
+        int age;
+        int weight;
+    public:
+        void eat(){
+            cout<<"Eating"<<endl;
+        }
+        int getAge(){
+            return this->Age;
+        }
+        void setAge(int age){
+            this->age = age;
+        }
+};
+int main(){
+
+}
