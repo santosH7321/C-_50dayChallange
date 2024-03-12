0758d800469f7d545c20834105a58b0ec71be96f
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-03-13 01:15:18 +0530

    Stack

---
+++ Stack/createStack.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main(){
+    //Creation
+    stack<int> st;
+
+    //insertion
+    st.push(10);
+    st.push(20);
+    st.push(30);
+
+    //size check
+    cout<<"Size of stack is: "<<st.size()<<endl;
+
+    //remove
+    st.pop();
+
+    //Check empty
+    if(st.empty()){
+        cout<<"Stack is empty"<<endl;
+    }
+    else{
+        cout<<"Stack is non empty"<<endl;
+    }
+    //top element
+    cout<<st.top()<<endl;
+}
